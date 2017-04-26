ae8820b0b2ee5f30b0ee8bc9b7d5316f921eda29
Yuji <hatsuneyuji@yahoo.co.jp>
2017-04-25 17:39:15 -0700

    changed

--- Package.swift
+++ Package.swift
@@ -2,7 +2,11 @@ import PackageDescription
 
 let package = Package(
     name: "xlibc",
+    targets: [
+        Target(name: "xlibc", dependencies: ["_xlibc"]),
+        Target(name: "_xlibc", dependencies: [])
+    ],
     dependencies: [
-    .Package(url: "https://github.com/projectSX0/_xlibc.git", majorVersion: 0)
+//    .Package(url: "https://github.com/projectSX0/_xlibc.git", majorVersion: 0)
     ]
 )
--- Sources/xlibc.swift
+++
@@ -1,8 +0,0 @@
-
-#if os(OSX) || os(iOS) || os(watchOS) || os(tvOS)
-@_exported import Darwin.C
-#else
-@_exported import Glibc
-#endif
-
-@_exported import _xlibc
